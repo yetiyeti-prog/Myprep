0ff1ad3acb5a9dfccaf5388f690b8b155e8ff08f
yetiyeti-prog <sammy.karmarkar@gmail.com>
2025-06-29 16:17:32 +0530

    added stars first code

---
+++ src/main/java/org/example/stars/stars_1.java
@@ -0,0 +1,31 @@
+package org.example.stars;
+
+public class stars_1 {
+
+
+    /*
+    *
+    * *
+    * * *
+    * * * *
+
+     */
+
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        int row = 1;
+
+        while (row <= n) {
+            int col = 1;
+            while(col <= row) {
+                System.out.print("*");
+                col ++;
+            }
+            System.out.println();
+            row ++;
+        }
+
+    }
+}
