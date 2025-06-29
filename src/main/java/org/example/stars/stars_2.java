c4c366504f2183b03df79b8211515556cf8ad7a9
yetiyeti-prog <sammy.karmarkar@gmail.com>
2025-06-29 16:49:58 +0530

    another stars

---
+++ src/main/java/org/example/stars/stars_2.java
@@ -0,0 +1,55 @@
+package org.example.stars;
+
+public class stars_2 {
+
+
+    /*
+    ****1****
+    ***212***
+    **32123**
+    *4321234*
+    543212345
+     */
+
+    public static void main(String[] args) {
+
+
+        int n = 5;
+
+        int row = 1;
+        while (row<=n) {
+
+            // for stars
+            int col = n-row;
+            while (col != 0) {
+                System.out.print("*");
+                col--;
+            }
+
+            // for numbers triangle 1
+            int col1 = row-1+1;
+            while (col1 != 0) {
+                System.out.print(col1);
+                col1--;
+            }
+
+            // for numbers triangle 2
+            int col2 = 2;
+            while (row != 1 && col2 != row+1) {
+                System.out.print(col2);
+                col2++;
+            }
+
+            // for stars
+            int col3 = n-row;
+            while (col3 != 0) {
+                System.out.print("*");
+                col3--;
+            }
+
+            System.out.println();
+            row ++;
+        }
+
+    }
+}
